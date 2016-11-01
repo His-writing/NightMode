2f02113c9989d461b0fca7e56efe07588f6db956
树振郭 <shuzhenguo@macBook-Pro.local>
2016-11-01 09:31:00 +0800

    Initial Commit

---
+++ NightMode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NightMode
+//
+//  Created by shuzhenguo on 2016/11/1.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
